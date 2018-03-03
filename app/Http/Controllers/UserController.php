17ff69211d54a6930be5720fdc1cdd1fae382310
Sr Developer <dev.l33@zoho.com>
2018-03-03 09:11:09 +0900

    - add users index page - add users navigation bar

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -112,4 +112,80 @@ class UserController extends Controller
             ], 500);
         }
     }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data['users'] = User::orderBy('created_at')->get();
+        return view('user.index', $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,14 +1,22 @@
 <nav id="sidebar" class="sidebar nav-collapse collapse">
     <ul id="side-nav" class="side-nav">
-        <li class="active">
+        <li>
             <a href="{{ route('users.dashboard') }}"><i class="fa fa-home"></i> <span class="name">Dashboard</span></a>
         </li>
-        <li class="panel ">
+        <li class="panel">
             <a class="accordion-toggle collapsed" data-toggle="collapse"
-               data-parent="#side-nav" href="#stats-collapse"><i class="fa fa-area-chart"></i> <span class="name">ICO</span></a>
-            <ul id="stats-collapse" class="panel-collapse collapse ">
+               data-parent="#side-nav" href="#ico-collapse"><i class="fa fa-area-chart"></i> <span class="name">ICO</span></a>
+            <ul id="ico-collapse" class="panel-collapse collapse ">
                 <li class=""><a href="{{ route('tokens.index') }}">List</a></li>
                 <li class=""><a href="{{ route('tokens.create') }}">Issue New</a></li>
+            </ul>
+        </li>
+        <li class="panel">
+            <a class="accordion-toggle collapsed" data-toggle="collapse"
+               data-parent="#side-nav" href="#users-collapse"><i class="fa fa-users"></i> <span class="name">Users</span></a>
+            <ul id="users-collapse" class="panel-collapse collapse ">
+                <li class=""><a href="{{ route('users.index') }}">List</a></li>
+                <li class=""><a href="{{ route('users.create') }}">Add New</a></li>
             </ul>
         </li>
     </ul>
