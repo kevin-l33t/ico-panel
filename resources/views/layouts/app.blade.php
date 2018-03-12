b4fad292f7b73198781f23c3a856d7b1ed003dcf
Sr Developer <dev.l33@zoho.com>
2018-03-12 18:31:26 +0900

    - update favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,6 @@
     <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.12.11/sweetalert2.min.css" />
     <link href="{{ asset('lib/cropperjs/cropper.min.css') }}" rel="stylesheet">
-    <link rel="shortcut icon" href="img/favicon.png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
