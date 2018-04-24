b5ed07143454719ae19e0dfd948581d95dab3bd4
Sr Developer <dev.l33@zoho.com>
2018-04-24 21:38:15 +0900

    - show verification status in user list

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -11,7 +11,8 @@
                     <th>Name</th>
                     <th class="no-sort">Email</th>
                     <th class="text-center">Role</th>
-                    <th class="text-center width-200">Date</th>
+                    <th class="text-center no-sort">KYC Verified</th>
+                    <th class="text-center width-200 hidden-xs">Date</th>
                     <th class="no-sort"></th>
                 </tr>
                 </thead>
@@ -20,9 +21,26 @@
                     <tr class="clickable-row" data-href="{{ route('users.edit', $user) }}">
                         <td>{{ $loop->index + 1 }}</td>
                         <td><span class="fw-semi-bold">{{ $user->name }}</span></td>
-                        <td>{{ $user->email }}</td>
+                        <td>
+                            {{ $user->email }}
+                            @if ($user->email_verified)
+                                <i class="fa fa-check"></i>
+                            @endif
+                        </td>
                         <td class="text-center">{{ $user->role->name }}</td>
-                        <td class="text-center">{{ $user->created_at }}</td>
+                        <td class="text-center">
+                            @switch($user->kyc_verified)
+                                @case(0)
+                                    <i class="glyphicon glyphicon-time"></i>
+                                    @break
+                                @case(1)
+                                    <i class="fa fa-check"></i>
+                                    @break
+                                @default
+                                    <i class="fa fa-ban"></i>
+                            @endswitch
+                        </td>
+                        <td class="text-center">{{ substr($user->created_at, 0, 10) }}</td>
                         <td class="text-right">
                             <a href="{{ route('users.edit', $user) }}" class="btn btn-primary btn-xs">
                                 <i class="fa fa-pencil"></i>
